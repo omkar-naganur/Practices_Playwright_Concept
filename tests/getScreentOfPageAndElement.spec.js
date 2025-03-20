0b15f76bc34cb5eaad79ba2650505c88a9bdd877
omkarn@krishworks.com <omkar@krishworks.com>
2025-03-20 14:34:59 +0530

    screent shot method added

---
+++ tests/getScreentOfPageAndElement.spec.js
@@ -0,0 +1,47 @@
+const { test, expect } = require("@playwright/test");
+
+test("@Webst Client App login", async ({ page }) => {
+  //js file- Login js, DashboardPage
+  const email = "omkarnaganur123@gmail.com";
+  const productName = "ZARA COAT 3";
+  const products = page.locator(".card-body");
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.getByPlaceholder("email@example.com").fill(email);
+  await page.getByPlaceholder("enter your passsword").fill("Pass@2025");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.waitForLoadState("networkidle");
+  await page.locator(".card-body b").first().waitFor();
+
+  await page
+    .locator(".card-body")
+    .filter({ hasText: "ZARA COAT 3" })
+    .getByRole("button", { name: "Add to Cart" })
+    .click();
+
+  await page
+    .locator(".card-body")
+    .filter({ hasText: "ZARA COAT 3" })
+    .getByRole("button", { name: "Add to Cart" })
+    .screenshot({ path: "cartButton.png" });
+
+  await page
+    .getByRole("listitem")
+    .getByRole("button", { name: "Cart" })
+    .click();
+
+  await page.screenshot({ path: "screenshot.png" });
+  await page.locator();
+
+  //await page.pause();
+  await page.locator("div li").first().waitFor();
+  await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+
+  await page.getByRole("button", { name: "Checkout" }).click();
+
+  await page.getByPlaceholder("Select Country").pressSequentially("ind");
+
+  await page.getByRole("button", { name: "India" }).nth(1).click();
+  await page.getByText("PLACE ORDER").click();
+
+  await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+});
